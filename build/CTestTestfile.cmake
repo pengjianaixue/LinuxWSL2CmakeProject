14f778c4ff8591c6e483637bdd466b616d92d82a
Jian Peng <pengjianaixue@hotmail.com>
2020-12-07 22:15:44 +0800

    WSL2 Cmake Project

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jianpeng/workspace/Cpp
-# Build directory: /home/jianpeng/workspace/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
