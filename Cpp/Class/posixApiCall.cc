cd0f53456f35cf99f2fd2621d42d09c0b0badba8
Jian Peng <pengjianaixue@hotmail.com>
2020-12-09 22:25:37 +0800

    add execteProgram

--- Cpp/Class/posixApiCall.cc
+++ Cpp/Class/posixApiCall.cc
@@ -4,3 +4,7 @@ extern "C" void executeApp(const char* programPath)
 {
     std::cout << programPath << std::endl;
 }
+extern "C" void executeProgram(const char* programPath)
+{
+    std::cout << programPath << std::endl;
+}
--- Cpp/Class/posixApiCall.h
+++ Cpp/Class/posixApiCall.h
@@ -2,4 +2,6 @@
 #include<atomic>
 #include<string>
 #include<iostream>
- extern "C" void executeApp(const char* programPath);
+
+extern "C" void executeApp(const char* programPath);
+extern "C" void executeProgram(const char* programPath);
