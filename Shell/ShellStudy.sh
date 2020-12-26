4c8abb7797d14cfe81f69f635d751f6a3bdabc70
Jian Peng <pengjianaixue@hotmail.com>
2020-12-26 10:42:01 +0800

    Add awk to Shell study

--- Shell/ShellStudy.sh
+++ Shell/ShellStudy.sh
@@ -3,7 +3,7 @@ tst="12"
 if [[ -z  ${tst} ]]; # 字符串为空
 then
     echo "empty"
-elif [[ -n  ${tst} ]]; # 字符串不为空 
+elif [[ -n ${tst} ]]; # 字符串不为空 
 then
      echo "not empty"
 fi
@@ -39,3 +39,23 @@ echo $lsecho
 #使用$(...)结构,提倡使用
 lsecho=$(ls -a)
 echo $lsecho
+
+#awk
+# awk 是包含一个BEGIN 语句 以及一个Pattern 以及一个 END 语句
+# BEGIN 语句和END语句都只会在开始 以及结束执行一次
+# BEGIN 和END 中间的语句 为 Pattern 语句，会在输入到awk 的每一行进行执行，所有非BEGIN 和 END 语句也都被视作Pattern 语句
+# $n 是代表输入行的域，即字段，默认为以空格分隔,$0 为整行， $1 为第一个字段，$2 为第二个，以此类推
+# awk 中的动作要用{} 进行包括，比如print 就是一个动作
+# NF 为 awk 内置 变量，表示当前的行的字段数
+# awk 也可以有条件控制if
+# Pattern 常用的还有正则表达式 匹配 形式了 ~ /regexp/
+
+# 条件控制
+awkprint=$(ls -lSa | awk 'BEGIN {print "Start\n ----------- "} {if(NF>2 && $5==4096) print $9} END {print "----------- \nend-of-report"}')
+# 正则表达式
+awkprint=$(ls -lSa | awk 'BEGIN {print "Start\n ----------- "} {if($0 ~/-rwxr-xr-x/) print $9} END {print "----------- \nend-of-report"}')
+echo "$awkprint"
+
+#tee 命令
+# 同时输出到文件和标准输出
+ls -lSa | tee test.txt
