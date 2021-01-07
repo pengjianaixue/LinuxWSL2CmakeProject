961aafcedbb130a614e14a997935ca9b97aec04d
Jian Peng <pengjianaixue@hotmail.com>
2021-01-07 17:33:45 +0800

    code clean

--- Python/VisbyBuild.py
+++ Python/VisbyBuild.py
@@ -6,12 +6,9 @@ import pexpect
 if __name__ == "__main__":
     print("Start........")
     child = pexpect.spawn("/home/eijpnae/toolScript/visbyAnd8863AutoBuildImp.sh",timeout=60*30)
-    # child = pexpect.spawn("ls -a",encoding='utf-8')
     child.logfile = sys.stdout
     child.expect("Is your radoisw repo under path /repo/eijpnae/radiosw")
     child.send("y\n")
     child.expect("#?")
     child.sendline("1")
     child.expect(pexpect.EOF)
-    # while child.isalive:
-    #     sleep(0.1)
