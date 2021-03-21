cae5771d2bf3e85d8a81ef8dd22cf16c3f7b4aec
Jian Peng <pengjianaixue@hotmail.com>
2021-03-21 11:38:48 +0800

    the poniter and the array test

--- Cpp/C/main.c
+++ Cpp/C/main.c
@@ -4,10 +4,14 @@
 
 int main()
 {
+    const char *strBuf = "this is buf";
+    strBuf = "which is reset the char pointer address";
     char buf[] = {"this is buf"};
+    buf[0] = 't';
     FILE* fileHandle = fopen("./Test.data","w+");
     printf("%ld\n", sizeof(buf));
     printf("%s\n", buf);
+    printf("%s\n", strBuf);
     if (fwrite(buf,sizeof(char),sizeof(buf)-1,fileHandle) <=0)
         printf("write failed!");
     return 0;
